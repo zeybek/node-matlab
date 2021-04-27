498a2fffb6a55beabd7b15dbfa7670a9708c0d77
Ahmet Zeybek <me@zeybek.dev>
2021-04-27 20:18:33 +0300

    initial

---
+++ index.js
@@ -0,0 +1,35 @@
+const { exec } = require("child_process");
+const tempFile = require("temp-write");
+
+const createTempFile = async (content) => {
+  return await tempFile(content, "temp.m");
+};
+/**
+ * Runs MATLAB script and gives output
+ *
+ * @param {string} script Runnable Script
+ * @returns result
+ *
+ */
+const run = async (script) => {
+  const temporaryFile = await createTempFile(script);
+  return new Promise((resolve, reject) => {
+    exec(
+      `matlab -nosplash -batch "run('${temporaryFile}'); exit;"`,
+      (error, stdout, stderr) => {
+        if (error) {
+          reject(stderr.trim());
+        }
+        resolve(stdout.trim());
+      }
+    );
+  });
+};
+
+run("3+4")
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
