9189cddb91a3b9463d5cdc0dc9799110bf627869
Ahmet Zeybek <me@zeybek.dev>
2021-04-28 11:58:09 +0300

    check version feature

--- index.js
+++ index.js
@@ -6,25 +6,58 @@ const createTempFile = async (content) => {
 };
 
 /**
- * Runs MATLAB script and gives output
- *
- * @param {string} script Runnable Script
- * @returns result
- *
+ * Checks MATLAB version
+ * @returns {number} version
  */
-const run = async (script) => {
-  const temporaryFile = await createTempFile(script);
+const getVersion = () => {
   return new Promise((resolve, reject) => {
-    exec(
-      `matlab -nosplash -batch "run('${temporaryFile}'); exit;"`,
-      (error, stdout, stderr) => {
-        if (error) {
-          reject(stderr.trim());
-        }
-        resolve(stdout.trim());
+    exec(`matlab -help`, (error, stdout, stderr) => {
+      let version = stdout
+        .trim()
+        .split("\r\n")
+        .reverse()[0]
+        .trim()
+        .match(/Version:(.*),/)[1]
+        .trim()
+        .split(".");
+
+      version.length -= 1;
+      version = parseFloat(version.join("."));
+
+      if (version < 9.2) {
+        reject("You must have installed MATLAB 2017a or later");
       }
-    );
+
+      resolve(version);
+    });
   });
 };
 
-module.exports = { run };
+/**
+ * Runs MATLAB script and gives output
+ * @param {string} script Runnable Script
+ * @returns {string} result
+ */
+const run = async (script) => {
+  try {
+    let version = await getVersion();
+    if (version) {
+      const temporaryFile = await createTempFile(script);
+      return new Promise((resolve, reject) => {
+        exec(
+          `matlab -nosplash -batch "run('${temporaryFile}'); exit;"`,
+          (error, stdout, stderr) => {
+            if (error) {
+              reject(stderr.trim());
+            }
+            resolve(stdout.trim());
+          }
+        );
+      });
+    }
+  } catch (error) {
+    return error;
+  }
+};
+
+module.exports = { run, getVersion };
